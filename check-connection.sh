92014b8ed6be93606684db8d9879119c0d228bd3
Timothy Smith <tsmith@saberastro.com>
2017-02-09 17:56:41 +1100

    Fix bugs with server response

--- check-connection.sh
+++ check-connection.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Checking internet connection. Will time out in $1"
+echo "Checking internet connection. Will timeout in $1"
 
-sleep $1
+t0=$(date +%s)
+
+while test $(date +%s) -le $((${t0} + $1))
+do
+    if ping -c 1 cuberider.com 1,2>>/dev/null
+    then
+        exit 0
+    fi
+done
 
 exit 1
