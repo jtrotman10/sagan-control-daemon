39abecd703829f78f1a292cc31d79a15eafcecbe
Timothy Smith <tsmith@saberastro.com>
2017-02-15 16:36:15 +1100

    Dont use shell=True

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ virtualenv -p python3 env
 . env/bin/activate
 pip install -r requirements.txt
 sudo cp hostapd.conf /etc/hostapd/
+sudo cp init.d_hostapd /etc/init.d/hostapd
 sudo cp udhcpd.conf /etc/
+sudo cp default_udhcpd /etc/default/udhcpd
 sudo cp sagan-control.service /etc/systemd/system/
