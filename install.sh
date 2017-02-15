7514b510cd70bbd166bde0c523005f6de07d7ff7
Timothy Smith <tsmith@saberastro.com>
2017-02-15 14:45:04 +1100

    Fix path for udhcpd.conf

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ sudo apt-get -y install hostapd udhcpd
 pwd
 pip3 install -r requirements.txt
 sudo cp hostapd.conf /etc/hostapd/
-sudo cp udhcpd.conf /etc/udhcpd/
+sudo cp udhcpd.conf /etc/
 sudo cp sagan-control.service /etc/systemd/system/
