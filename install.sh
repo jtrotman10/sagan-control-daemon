08f240c4151645d54d6a1b2096ad26cc3c65277b
Timothy Smith <tsmith@saberastro.com>
2017-02-15 15:19:24 +1100

    use dot instead of source

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt-get -y install hostapd udhcpd
 sudo pip3 install virtualenv
 virtualenv -p python3 env
-source env/bin/activate
+. env/bin/activate
 pip install -r requirements.txt
 sudo cp hostapd.conf /etc/hostapd/
 sudo cp udhcpd.conf /etc/
