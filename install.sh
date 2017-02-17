daa54dc55d732a663d9430e3b5c4d026c0a2d774
Timothy Smith <tsmith@saberastro.com>
2017-02-17 11:37:04 +1100

    only make virtual env when necessary

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 set -e
 sudo apt-get -y install hostapd udhcpd
 sudo pip3 install virtualenv
-virtualenv -p python3 env
+# check there is a readable file called env/bin/activate
+if [ ! -r env/bin/activate ]; then
+    virtualenv -p python3 env
+fi
 . env/bin/activate
 pip install -r requirements.txt
 sudo mkdir sandbox
