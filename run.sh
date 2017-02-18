36fa396eeb55c0485b9daa38d8fed486f403cb18
Timothy Smith <tsmith@saberastro.com>
2017-02-18 19:55:15 +1100

    cd to working dir

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
+cd /opt/sagan-control-daemon/
 while true; do
-    /opt/sagan-control-daemon/env/bin/python sagan-control-daemon.py config.json
+    env/bin/python sagan-control-daemon.py config.json
     sleep 10s
 done
