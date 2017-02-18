00653dff1973951d929f0e51fb7fdef7f7cebf42
Timothy Smith <tsmith@saberastro.com>
2017-02-18 18:43:53 +1100

    use rc_local to run sagan control

--- install.sh
+++ install.sh
@@ -16,10 +16,11 @@ if [ ! -d sandbox ]; then
     sudo mkdir sandbox
     sudo chown pi:pi sandbox
 fi
+sudo cp rc_local.txt /etc/rc.local
+sudo chmod +x /etc/rc.local
 sudo cp hostapd.conf /etc/hostapd/
 sudo cp init.d_hostapd /etc/init.d/hostapd
 sudo cp udhcpd.conf /etc/
 sudo cp default_udhcpd /etc/default/udhcpd
-sudo cp sagan-control.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl start sagan-control
+
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+while 1; do
+    env/bin/python sagan-control-daemon.py config.json
+done
