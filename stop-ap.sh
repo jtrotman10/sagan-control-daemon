83ecdb4705cb7c05b46b96a149973a5cc307ef98
Timothy Smith <tsmith@saberastro.com>
2017-03-01 15:31:17 +1100

    Update start/stop scripts

--- stop-ap.sh
+++ stop-ap.sh
@@ -4,7 +4,7 @@ set -e
 interface=$1
 
 sudo ifconfig ${interface} 0.0.0.0
-sudo service udhcpd stop
+sudo service dnsmasq stop
 sudo service hostapd stop
 sudo service dhcpcd start
 sudo ifup ${interface}
