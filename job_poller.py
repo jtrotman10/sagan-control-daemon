79851ec25ab20642f520c2fbab466d704f57b312
AlexCollins <Alexander@MacBook-Pro.local>
2017-03-03 11:59:35 +1100

    fix

--- job_poller.py
+++ job_poller.py
@@ -363,7 +363,7 @@ class Poller:
     def end_experiment(self):
         self.out_thread.join()
 
-        if self.using_sagan == True:
+        if self.using_sagan == False:
             # ensure fifo is not hanging
             print("(end_experiment) opening fifo incase of sagan not used")
             fifo_file = open(_TELEMETRY_PIPE_PATH, 'w')
