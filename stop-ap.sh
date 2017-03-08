456fc2abdcf5a11e02e3c9b225e581c096144216
Timothy Smith <tsmith@saberastro.com>
2017-03-09 10:27:16 +1100

    Hack to try to get interface up more reliably after tearing down ap.

--- stop-ap.sh
+++ stop-ap.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
-set -e
 
 interface=$1
 
-sudo ifconfig ${interface} 0.0.0.0
 sudo service dnsmasq stop
 sudo service hostapd stop
+sudo ifconfig ${interface} 0.0.0.0
 sudo service dhcpcd start
-sleep 2
-sudo ifup ${interface}
+
+# Sometimes there is an error bringing up the interface.
+# Try toggling it a few times to get it going. :S
+i=0
+while [ ${i} -le 10 ]; do
+    sudo ifdown ${interface}
+    if sudo ifup ${interface}; then
+        exit 0;
+    fi
+    i=$((${i} + 1))
+done
