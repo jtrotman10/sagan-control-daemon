779dde27fc90c708f87a47fb009a80ac479aaaa1
Timothy Smith <tsmith@saberastro.com>
2017-03-08 12:20:22 +1100

    Record output in log.txt

--- rc_local.sh
+++ rc_local.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 printf "Enabling remote experiments"
 rm /opt/sagan-control-daemon/*.pid
+rm /opt/sagan-control-daemon/log.txt
+
 if [ -e /opt/sagan-control-daemon/enabled ]; then
     /opt/sagan-control-daemon/start.sh
 fi
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd /opt/sagan-control-daemon/
 
 while true; do
     echo "~" > leds
-    env/bin/python sagan-control-daemon.py config.json
+    env/bin/python sagan-control-daemon.py config.json &> log.txt
     echo "~" > leds
     sleep 10s
 done
