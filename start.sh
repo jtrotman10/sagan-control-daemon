0d153430eb69d8893b159b7ca2a05674c57f5bfd
Timothy Smith <tsmith@saberastro.com>
2017-03-08 19:36:52 +1100

    hide nohup messages

--- start.sh
+++ start.sh
@@ -12,14 +12,14 @@ cd /opt/sagan-control-daemon
 touch enabled
 
 if [ ! -e run-notifier.pid ]; then
-    nohup ./run-notifier.sh &
+    nohup ./run-notifier.sh >/dev/null 2>&1 &
     run_notifier_pid=$!
     disown ${run_notifier_pid}
     echo ${run_notifier_pid} > run-notifier.pid
 fi
 
 if [ ! -e run.pid ]; then
-    nohup ./run.sh &
+    nohup ./run.sh >/dev/null 2>&1 &
     run_pid=$!
     disown ${run_pid}
     echo ${run_pid} > run.pid
