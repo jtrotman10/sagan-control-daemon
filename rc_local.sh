6f1b7cdd768e90903255e70bd403f52f590e68f1
Timothy Smith <tsmith@saberastro.com>
2017-03-09 17:00:50 +1100

    remove errors

--- rc_local.sh
+++ rc_local.sh
@@ -2,6 +2,7 @@
 echo "Enabling remote experiments"
 rm /opt/sagan-control-daemon/*.pid
 rm /opt/sagan-control-daemon/log.txt
+rm /opt/sagan-control-daemon/errors.txt
 
 if [ -e /opt/sagan-control-daemon/enabled ]; then
     /opt/sagan-control-daemon/start.sh
